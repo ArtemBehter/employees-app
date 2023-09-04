77b93bcda588b4b34b7a4f94ce69f11268e3e3b1
ArtemBehter <www.behchik@gmail.com>
2023-09-04 20:37:50 +0200

    Refactoring code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,20 +9,20 @@ import './app.css';
 function App() {
 
     const data = [
-        {name: 'John C.', salary: 800},
-        {name: 'Alex M.', salary: 3000},
-        {name: 'Carl W.', salary: 5000},
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3},
     ];
 
     return (
         <div className="app">
-            <AppInfo/>
+            <AppInfo />
 
             <div className="search-panel">
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-
+            
             <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
